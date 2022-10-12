2be45cca3d09f017a0327413902c9c1c7c5deb08
Phimlaphat Surayaem <fernphimlaphat@gmail.com>
2022-10-12 15:39:23 +0700

    1 rec & square --> LSP

---
+++ LAB/LAB Solid/1_rectangle&Square/Main.java
@@ -0,0 +1,26 @@
+package com.solid.lsp;
+
+
+public class Main {
+
+    private static int width = 4;
+    private static int height = 5;
+    private static int side = 5;
+
+    
+    public static void main(String[] args) {
+        Rectangle reg1 = new Rectangle(width, height);
+        System.out.println("==== Rectangle ====");
+        System.out.println("Width = " + width);
+        System.out.println("Height = " + height);
+        System.out.println("Area = " + reg1.getArea());
+
+        Rectangle reg2 = new Square(side);
+        reg2.setWidth(side);
+        reg2.setHeight(side);
+        System.out.println("====   Square  ====");
+        System.out.println("Width = " + side);
+        System.out.println("Height = " + side);
+        System.out.println("Area = " + reg2.getArea());
+    }
+}
---
+++ LAB/LAB Solid/1_rectangle&Square/Square.java
@@ -0,0 +1,48 @@
+package com.solid.lsp;
+
+class Square extends Rectangle {
+    Square(int side) {
+        this.setSide(side);
+    }
+
+    @Override
+    public void setWidth(int width) {
+        this.setSide(width);
+    }
+
+    @Override
+    public void setHeight(int height) {
+            this.setSide(height);
+            
+        
+    }
+
+    public void setSide(int side) {
+        super.setWidth(side);
+        super.setHeight(side);
+    }
+}
+
+/*
+ * public class Square extends Rectangle {
+ * 
+ * Square(int side) {
+ * this.setSide(side);
+ * }
+ * 
+ * @Override
+ * public void setWidth(int width) {this.setSide(width);
+ * }
+ * 
+ * @Override
+ * public void setHeight(int height) {
+ * this.setSide(height);
+ * }
+ * 
+ * public void setSide(int side) {
+ * super.setWidth(side);
+ * super.setHeight(side);
+ * 
+ * }
+ * }
+ */
